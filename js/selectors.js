d17413550f94290c808166fa03af58d211ab05a2
sophiemaniscalco <sophmaniscalco@gmail.com>
2018-06-22 18:59:19 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,22 @@
 'use-strict';
 //add your code here
+function h1Selector(){
+  return $('h1')
+}
+
+function liInOlSelector(){
+  document.querySelectorAll("ol li");
+ return document.querySelectorAll("ol li");
+}
+
+function linkSelector(){
+  return $('a', '#box4', '.box5')
+}
+
+function imageSelector() {
+  return $("img[alt='cat sleeping']")
+}
+
+function checkboxInputSelector() {
+  return $(":checkbox")
+}
